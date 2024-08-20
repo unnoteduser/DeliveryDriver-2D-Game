5d2858b1cb7896ffaff214f97251eb1eb4c3abcc
Pavel Andreyeu <pavel_andreyeu@belkatechnologies.com>
2024-08-20 15:36:33 +0200

    Initial commit

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    [SerializeField] float destroyDelay = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+    SpriteRenderer spriteRenderer;
+    bool hasPackage;
+    private void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("Ouchh");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package picked up!");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+        }
+        else if(other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package delivered!");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+    }
+}
